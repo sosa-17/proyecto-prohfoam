590a56e20a1c28d05bed8d6b059ae4696f6ddb57
Samuel Sosa <samuelsosaaleman@gmail.com>
2019-03-17 00:28:03 -0600

    base con factura

--- basenueva123.sql
+++ basenueva123.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-03-2019 a las 05:36:55
+-- Tiempo de generación: 17-03-2019 a las 07:22:47
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 5.6.40
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `basenueva`
+-- Base de datos: `basenueva123`
 --
 
 -- --------------------------------------------------------
@@ -38,9 +38,9 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `name`) VALUES
-(3, 'Mueble Dormitorio'),
-(2, 'Muebles Oficina'),
-(1, 'Muebles Sala');
+(1, 'Mueble Dormitorio'),
+(2, 'Mueble Oficina'),
+(3, 'Mueble Sala');
 
 -- --------------------------------------------------------
 
@@ -60,10 +60,8 @@ CREATE TABLE `datospersonales` (
 --
 
 INSERT INTO `datospersonales` (`id`, `email`, `direccion`, `usuarios_id`) VALUES
-(1, 'juan123@gmail.com', 'coma', 4),
-(13, 'sosaa@unah.hn', 'villa', 20),
-(14, 'samuelsosaaleman@gmail.com', 'centro', 21),
-(15, 'aleman@unah.hn', 'honduras', 22);
+(16, 'samer@gmail.com', 'tgu, villa olimpica', 23),
+(17, 'tito@gmail.com', 'tgu, Centro', 24);
 
 -- --------------------------------------------------------
 
@@ -77,6 +75,67 @@ CREATE TABLE `direccion` (
   `Ciudad` varchar(50) NOT NULL,
   `Bloque` varchar(25) NOT NULL,
   `Num_Casa` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `direccion`
+--
+
+INSERT INTO `direccion` (`id_direccion`, `Colonia`, `Ciudad`, `Bloque`, `Num_Casa`) VALUES
+(1, 'Trapiche', 'TGU', 'p', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `empresa_id` int(11) NOT NULL,
+  `nombre` varchar(250) DEFAULT NULL,
+  `RTN` int(11) DEFAULT NULL,
+  `id_direccion` int(11) UNSIGNED NOT NULL,
+  `telefono` varchar(30) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`empresa_id`, `nombre`, `RTN`, `id_direccion`, `telefono`, `email`) VALUES
+(1, 'PROH-FOAM', 8011995, 1, '+504 22758935', 'prohfoam@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `factura_id` int(11) NOT NULL,
+  `numero_factura` int(11) DEFAULT NULL,
+  `fecha_factura` datetime DEFAULT NULL,
+  `usuarios_id` int(11) UNSIGNED NOT NULL,
+  `id_empresa` varchar(45) DEFAULT NULL,
+  `empresa_empresa_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura_detalles`
+--
+
+CREATE TABLE `factura_detalles` (
+  `factura_detalles_id` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio_unitario` decimal(10,0) DEFAULT NULL,
+  `isv` decimal(10,0) DEFAULT NULL,
+  `sub_total` decimal(10,0) DEFAULT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `productos_id` int(11) UNSIGNED NOT NULL,
+  `factura_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -98,11 +157,8 @@ CREATE TABLE `grupo_usuario` (
 
 INSERT INTO `grupo_usuario` (`id`, `nombre_grupo`, `nivel_grupo`, `estado_grupo`) VALUES
 (1, 'Admin', 1, 1),
-(2, 'Special', 2, 0),
-(3, 'User', 3, 1),
-(7, 'especial', 5, 0),
-(8, 'funciona', 7, 1),
-(10, 'sirve', 8, 0);
+(12, 'Vendedor', 2, 1),
+(13, 'Clientes', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -121,7 +177,7 @@ CREATE TABLE `media` (
 --
 
 INSERT INTO `media` (`id`, `file_name`, `file_type`) VALUES
-(1, '6.jpg', 'image/jpeg'),
+(1, '	\r\n6.jpg', 'image/jpeg'),
 (2, '12.jpg', 'image/jpeg'),
 (3, '18.jpg', 'image/jpeg'),
 (4, '7.jpg', 'image/jpeg');
@@ -148,11 +204,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `name`, `cantidad`, `precio_compra`, `precio_venta`, `categoria_id`, `media_id`, `date`) VALUES
-(1, 'prueba', '0', '1500.00', '1600.00', 1, 1, '2019-03-05 10:59:00'),
-(2, 'prueba2', '15', '1500.00', '1800.00', 2, 2, '2019-03-05 11:20:16'),
-(3, 'prueba3', '10', '1500.00', '1600.00', 1, 3, '2019-03-05 10:55:50'),
-(4, 'samul', '5', '1400.00', '1800.00', 1, 4, '2019-03-05 11:23:03'),
-(5, 'prueba 3', '10', '2000.00', '2700.00', 3, 4, '2019-03-05 10:54:59');
+(10, 'Cama', '4', '147.00', '200.00', 1, 2, '2019-03-17 07:10:54'),
+(11, 'Silla', '47', '78.00', '100.00', 2, 4, '2019-03-17 07:11:26'),
+(12, 'Colchon', '4', '17.00', '20.00', 3, 1, '2019-03-17 07:12:10');
 
 -- --------------------------------------------------------
 
@@ -176,13 +230,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `name`, `nombre_usuario`, `password`, `nivel_usuario`, `image`, `estado`, `ultimo_acceso`) VALUES
-(1, 'Admin usuarios', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'pzg9wa7o1.jpg', 1, '2019-03-10 05:24:20'),
-(2, 'Special User', 'special', 'ba36b97a41e7faf742ab09bf88405ac04f99599a', 2, 'no_image.jpg', 1, '2017-06-16 07:11:26'),
-(3, 'Default User', 'user', '12dea96fec20593566ab75692c9949596833adc9', 3, 'no_image.jpg', 1, '2017-06-16 07:11:03'),
-(4, 'juan carlos', 'juanca', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, 'no_image.jpg', 1, NULL),
-(20, 'sosa', 'sosa', '959d3d631fb6f17f1b07f751372041a1edb210d1', 1, 'no_image.jpg', 1, NULL),
-(21, 'samuel', 'samuel', 'c16aab9fe3288df0fb8fc1d24990a300b6b8f299', 1, 'no_image.jpg', 1, NULL),
-(22, 'loco', 'loco', '7163d28263e69194a23cc96dde29dd92886fb034', 1, 'no_image.jpg', 1, NULL);
+(1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'no_image.jpg', 1, '2019-03-17 07:13:06'),
+(23, 'Samer', 'Samer', 'd530ef8ba7401d025c6e5b5d791fb1ed03ad7de9', 2, 'no_image.jpg', 1, NULL),
+(24, 'Tito', 'tito', '1a96f9437697ef43237868412d77b15991964f6e', 3, 'no_image.jpg', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -203,7 +253,7 @@ CREATE TABLE `ventas` (
 --
 
 INSERT INTO `ventas` (`id`, `producto_id`, `cant`, `precio`, `date`) VALUES
-(1, 1, 2, '3200.00', '2019-03-05');
+(2, 10, 1, '200.00', '2019-03-17');
 
 --
 -- Índices para tablas volcadas
@@ -229,6 +279,29 @@ ALTER TABLE `datospersonales`
 --
 ALTER TABLE `direccion`
   ADD PRIMARY KEY (`id_direccion`);
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`empresa_id`,`id_direccion`),
+  ADD KEY `fk_empresa_direccion1_idx` (`id_direccion`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`factura_id`),
+  ADD KEY `fk_factura_usuarios1_idx` (`usuarios_id`),
+  ADD KEY `fk_factura_empresa1_idx` (`empresa_empresa_id`);
+
+--
+-- Indices de la tabla `factura_detalles`
+--
+ALTER TABLE `factura_detalles`
+  ADD PRIMARY KEY (`factura_detalles_id`),
+  ADD KEY `fk_factura_detalles_productos1_idx` (`productos_id`),
+  ADD KEY `fk_factura_detalles_factura1_idx` (`factura_id`);
 
 --
 -- Indices de la tabla `grupo_usuario`
@@ -276,49 +349,67 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `datospersonales`
 --
 ALTER TABLE `datospersonales`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `direccion`
 --
 ALTER TABLE `direccion`
-  MODIFY `id_direccion` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_direccion` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `empresa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `factura_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura_detalles`
+--
+ALTER TABLE `factura_detalles`
+  MODIFY `factura_detalles_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `grupo_usuario`
 --
 ALTER TABLE `grupo_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `media`
 --
 ALTER TABLE `media`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -329,6 +420,26 @@ ALTER TABLE `ventas`
 --
 ALTER TABLE `datospersonales`
   ADD CONSTRAINT `FK_datosPersonales` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD CONSTRAINT `fk_empresa_direccion1` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id_direccion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `fk_factura_empresa1` FOREIGN KEY (`empresa_empresa_id`) REFERENCES `empresa` (`empresa_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_factura_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `factura_detalles`
+--
+ALTER TABLE `factura_detalles`
+  ADD CONSTRAINT `fk_factura_detalles_factura1` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`factura_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_factura_detalles_productos1` FOREIGN KEY (`productos_id`) REFERENCES `productos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `productos`
