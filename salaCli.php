48e85c0a3d3dede0203cbebe8c88d546c4e9cf23
Samuel Sosa <samuelsosaaleman@gmail.com>
2019-03-18 23:04:02 -0600

    catalogo de la parte del cliente

---
+++ salaCli.php
@@ -0,0 +1,64 @@
+<?php
+    require_once('includes/sql.php');
+    $sql="SELECT name,id FROM Categorias";
+    $resultado=find_by_sql($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Catalogo PROHFOAM</title>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/shop-homepage.css" rel="stylesheet">
+</head>
+<body>
+  <?php include_once('layouts/header.php'); ?>
+<div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <h1 class="my-4">Catalogo</h1>
+        <div class="list-group">
+          <?php foreach($resultado as $resu): ?>
+            <?php if($resu['id'] === '1'): ?>
+              <a class="list-group-item" href="salaCli.php"><?php echo $resu['name']; ?></a>
+            <?php endif;?>
+            <?php if($resu['id'] === '2'): ?>
+              <a class="list-group-item" href="dormitorioCli.php"><?php echo $resu['name']; ?></a>
+            <?php endif;?>
+            <?php if($resu['id'] === '3'): ?>
+              <a class="list-group-item" href="oficinaCli.php"><?php echo $resu['name']; ?></a>
+            <?php endif;?>
+          <?php endforeach;?>
+        </div>
+      </div>
+      
+   <div class="col-lg-9">
+   </br>
+    <div class="row" id="div-categoriasala">
+          
+            
+          
+           
+          
+          </div>
+        </div>
+  </div>
+</div>  
+</div>
+<br/>
+<br/>
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Ingeniera en Sistemas UNAH 2019</p>
+    </div>
+  </footer>
+ </body>
+ <script src="vendor/jquery/jquery.min.js"></script>
+ <script src="css/bootstrap.bundle.min.js"></script>
+ <script src="js/imagenes.js"></script>
+   
+</html>
+<?php include_once('layouts/footer.php'); ?>
